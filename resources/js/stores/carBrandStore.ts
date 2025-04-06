494983a067a2541c8d6cf77d64e5185d9aff810d
Viktor <filipcseiviktor@gmail.com>
2025-04-06 23:25:09 +0200

    precommit hook test

--- resources/js/stores/carBrandStore.ts
+++ resources/js/stores/carBrandStore.ts
@@ -1,42 +1,45 @@
-import { defineStore } from "pinia";
-import axios from "axios";
-import { CarBrand } from "../types/CarBrand";
+import { defineStore } from 'pinia'
+import axios from 'axios'
+import { CarBrand } from '../types/CarBrand'
 
-export const useCarBrandStore = defineStore("carBrandStore", {
+export const useCarBrandStore = defineStore('carBrandStore', {
     state: () => ({
         brands: [] as CarBrand[],
     }),
     actions: {
         async index() {
             try {
-                const { data } = await axios.get<{ data: CarBrand[] }>("/api/v1/car-brands");
-                this.brands = data.data;
+                const { data } = await axios.get<{ data: CarBrand[] }>('/api/v1/car-brands')
+                this.brands = data.data
             } catch (error) {
-                throw error;
+                throw error
             }
         },
+
         async store(name: string) {
             try {
-                const { data } = await axios.post<{ data: CarBrand }>("/api/v1/car-brands", { name });
-                return data.data;
+                const { data } = await axios.post<{ data: CarBrand }>('/api/v1/car-brands', {
+                    name,
+                })
+                return data.data
             } catch (error) {
-                throw error;
+                throw error
             }
         },
         async edit(id: number, name: string) {
             try {
-                const { data } = await axios.put(`/api/v1/car-brands/${id}`, { name });
-                return data.data;
+                const { data } = await axios.put(`/api/v1/car-brands/${id}`, { name })
+                return data.data
             } catch (error) {
-                throw error;
+                throw error
             }
         },
         async delete(id: number) {
             try {
-                await axios.delete(`/api/v1/car-brands/${id}`);
+                await axios.delete(`/api/v1/car-brands/${id}`)
             } catch (error) {
-                throw error;
+                throw error
             }
-        }
-    }
-});
+        },
+    },
+})
